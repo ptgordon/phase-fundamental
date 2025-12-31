50dfcea7b5d85af3b45dab4bd0d5fe807013e46c
Patrick Gordon <pgordon8001@gmail.com>
2025-12-31 07:57:23 -0600

    commented and created some better variable names

--- main.py
+++ main.py
@@ -2,42 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-f0 = 1_000        # phasor frequency (Hz)
-fs = 123          # phase sampling rate (Hz)
-phi0 = 0.0         # initial phase (rad)
+f0 = 20_000          # phasor frequency (Hz)
+fs = 283            # phase sampling rate (Hz)
+phi0 = 0.0          # initial phase (rad)
 duration = 1        # seconds
 
-p0 = 1/f0
-ps = 1/fs
+p0 = 1/f0           # period of sampled phasor
+ps = 1/fs           # period of sampler
 
-rotations_per_sample = ps/p0 - np.floor(ps/p0)
+# we are going to first find the aliasing
+rotations_per_sample = ps/p0 
+observed_rotations_per_sample = rotations_per_sample- np.floor(rotations_per_sample)
 
-rotations = rotations_per_sample
+observed_rotations = observed_rotations_per_sample
 iters = 1
 
-if rotations_per_sample > .5:
-    while rotations <= rotations_per_sample:
-        rotations = rotations + rotations_per_sample
-        rotations = rotations - np.floor(rotations)
-        iters = iters + 1
-else:
-    while rotations >= rotations_per_sample:
-        rotations = rotations + rotations_per_sample
-        rotations = rotations - np.floor(rotations)
-        iters = iters + 1
+# Next we will figure out where the aliasing... aliases
+while observed_rotations <= observed_rotations_per_sample:
+    rotations = observed_rotations + observed_rotations_per_sample
+    observed_rotations = rotations - np.floor(rotations)
+    iters = iters + 1
 
-print(f"{iters}")
+print(f"points before inner wrap: {iters}")
 
-next_peak = rotations_per_sample * iters
-next_peak = next_peak - np.floor(next_peak)
+next_corr_point = observed_rotations_per_sample * iters
+next_corr_point = next_corr_point - np.floor(next_corr_point)
 
-peak_movement = next_peak - rotations_per_sample
-rec_peak_movement = 1/peak_movement
+corr_point_movement = next_corr_point - observed_rotations_per_sample
+rec_corr_point_movement = 1/corr_point_movement
 
-bad_harmonic = fs/rec_peak_movement
+block_wrap = fs/rec_corr_point_movement
 
-print(f"{bad_harmonic}")
-
+print(f"observed wraps of blocks: {block_wrap}")
 
 # Time samples
 t = np.arange(0, duration, 1/fs)
