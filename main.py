22cce330dec5cec4024c3cf1b055c4e0513e97be
Patrick Gordon <pgordon8001@gmail.com>
2026-01-05 23:35:41 -0600

    I have no clue what I'm doing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sys
 def main():
 # Parameters
     f0 = 20_000          # phasor frequency (Hz)
-    fs = 283            # phase sampling rate (Hz)
-    phi0 = 0.0          # initial phase (rad)
+    fs = 283            # phases sampling rate (Hz)
+    phi0 = 0.0          # initial phases (rad)
     duration = 1        # seconds
 
     if len(sys.argv) > 2:
@@ -15,48 +15,39 @@ def main():
     else:
         print("no arguments provided, using defaults of 20 kHz and 283 Hz sample")
 
-    p0 = 1/f0           # period of sampled phasor
-    ps = 1/fs           # period of sampler
+    t = np.arange(0, duration, 1/fs)
+    phases = np.mod(2*np.pi*f0*t + phi0 + np.pi, 2*np.pi) - np.pi
+    # phases = np.mod(2*np.pi*f0*t, 2*np.pi)
+    print(f"{phases}")
 
-# we are going to first find the aliasing
-    rotations_per_sample = ps/p0 
-    observed_rotations_per_sample = rotations_per_sample - int(rotations_per_sample)
-
-    print(f"observed rotations per sample: {observed_rotations_per_sample}")
-
-    observed_rotations = observed_rotations_per_sample
     iters = 1
+    first_phase = phases[1]
 
-# Next we will figure out where the aliasing... aliases
-    while abs(observed_rotations - 0.5) >= abs(observed_rotations_per_sample - 0.5):
-        rotations = observed_rotations + observed_rotations_per_sample
-        observed_rotations = rotations - int(rotations)
-        print(f"{observed_rotations}")
-        iters = iters + 1
+    for phase in phases[1:]:
+        iters = iters+1
+        if abs(phase) < abs(first_phase):
+            break
 
     print(f"points before inner wrap: {iters}")
 
-    next_corr_point = observed_rotations_per_sample * (iters+1)
-    next_corr_point = next_corr_point - int(next_corr_point)
-    print (f"next corresponding point: {next_corr_point}")
+    next_corr_point = phases[iters-1]
+    print(f"{next_corr_point}")
 
-    corr_point_movement = next_corr_point - observed_rotations_per_sample
-    rec_corr_point_movement = 1/corr_point_movement
+    corr_point_movement = abs(phases[1]) - abs(next_corr_point) 
+
+    rec_corr_point_movement = phases[1]/(np.pi/corr_point_movement)
 
     block_wrap = fs/rec_corr_point_movement
 
     print(f"observed wraps of blocks: {block_wrap}")
 
 # Time samples
-    t = np.arange(0, duration, 1/fs)
     colors = ['cyan'] * len(t)
 
-    for i in range(0, len(t), iters+1):
+    for i in range(0, len(t), iters-1):
         colors[i] = 'red'
 
-# Phase samples (wrapped to [-pi, pi])
-    phase = np.mod(2*np.pi*f0*t + phi0 + np.pi, 2*np.pi) - np.pi
-    # phase = np.mod(2*np.pi*f0*t, 2*np.pi)
+# phases samples (wrapped to [-pi, pi])
 
 # Plot
     fig, ax = plt.subplots()
@@ -64,9 +55,9 @@ def main():
     fig.patch.set_facecolor("black")     # figure background
     ax.set_facecolor("black")            # axes background
 
-    ax.scatter(t, phase, c = colors, marker='x')
+    ax.scatter(t, phases, c = colors, marker='x')
 
-    ax.plot(t, phase, c = 'cyan')
+    ax.plot(t, phases, c = 'cyan')
 
     ax.tick_params(colors="white")
     ax.xaxis.label.set_color("white")
@@ -74,8 +65,8 @@ def main():
     ax.title.set_color("white")
 
     ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Phase (rad)")
-    ax.set_title(f"Sampled Phase of {f0/1000:.1f} kHz Phasor at {fs} Hz")
+    ax.set_ylabel("phases (rad)")
+    ax.set_title(f"Sampled phases of {f0/1000:.1f} kHz Phasor at {fs} Hz")
     ax.grid(True)
 
     plt.show()
